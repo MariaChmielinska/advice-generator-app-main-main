d96639db7fd498bfb25fc09e3e6004d007d8d29a
MariaChmielinska <maria.anna.chmielinska@gmail.com>
2024-05-16 19:28:16 +0200

    solution object in object

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
 // ¡Buena suerte!
+
+//1. API: Object in Object. propiedades advice and slip.id
+//2. {slip_id} to return of every advice 
+
+
+const button = document.querySelector(".card__dice");
+const adviceId = document.querySelector("#advice-id");
+const quote = document.querySelector(".card__quote");
+
+button.addEventListener("click", async function(){
+
+   const response = await fetch("https://api.adviceslip.com/advice");
+   console.log("object response check API OBJ", response);
+
+   const data = await response.json();
+   console.log("OBJECT DATA", data);
+
+quote.textContent = data.slip.advice;
+
+adviceId.textContent = data.slip.id;
+
+
+})
